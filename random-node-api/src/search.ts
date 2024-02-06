b20b3a0c4401d09754b7dc8fdbaf86c6cfc97c23
Jimmy Cerone <67113663+jimmyceroneii@users.noreply.github.com>
2024-02-06 03:58:14 +0000

    add in new var for api key

--- random-node-api/src/search.ts
+++ random-node-api/src/search.ts
@@ -1,10 +1,12 @@
 import Metaphor from 'metaphor-node'
 
 export const getResultsAndContent = async (query: string) => {
-  const metaphor = new Metaphor('your_api_key')
+  const metaphor = new Metaphor(process.env.EXA_API_KEY || '')
 
   const searchResponse = await metaphor.search(query)
   const contents = await metaphor.getContents(searchResponse.results)
 
   return contents.contents
 }
+
+console.log(getResultsAndContent('help me find the best articles on ai'));
