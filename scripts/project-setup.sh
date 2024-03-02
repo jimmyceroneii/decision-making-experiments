09f714f542fca34982d9e74c62837cb4f50bcccd
Jimmy Cerone <67113663+jimmyceroneii@users.noreply.github.com>
2024-03-02 08:25:01 +0000

    add script to setup ts node project

---
+++ scripts/project-setup.sh
@@ -0,0 +1,9 @@
+yarn init -y
+
+yarn add --dev typescript ts-node @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier prettier
+
+npx tsc -init
+
+cp ../random-product-node/.prettierrc .
+
+cp ../random-product-node/.eslintrc.js .
