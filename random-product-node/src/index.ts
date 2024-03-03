cf93a0f9719ef767f7ea081e23c0c48c91ae1912
Jimmy Cerone <67113663+jimmyceroneii@users.noreply.github.com>
2024-03-03 06:27:27 +0000

    update file path to work with action

--- random-product-node/src/index.ts
+++ random-product-node/src/index.ts
@@ -3,18 +3,19 @@ import { shuffleList } from './randomizer';
 import { getSimilar, searchForContent } from './search';
 
 const main = async () => {
-  const filePath: string = __dirname + '/products.txt'
+  const filePath: string = 'src/products.txt'
 
   try {
     const fileContent: string = fs.readFileSync(filePath, 'utf-8')
     const products: string[] = fileContent.split('\n')
 
-    const randomProduct = shuffleList(products)[0]
+    const randomProduct = shuffleList(products)[0];
+
+    console.log(randomProduct);
 
     const sources = await searchForContent(`Here is some information about the following product: ${randomProduct}`);
     const similar = await getSimilar(randomProduct);
-
-    console.log(randomProduct);
+    
     console.log(sources);
     console.log(similar);
   } catch (error) {
