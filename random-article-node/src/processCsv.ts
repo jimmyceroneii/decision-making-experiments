9e7908464b03d4407c7be4a35ce402366a74346b
Jimmy Cerone <67113663+jimmyceroneii@users.noreply.github.com>
2024-03-22 04:41:20 +0000

    WIP validation on csv

--- random-article-node/src/processCsv.ts
+++ random-article-node/src/processCsv.ts
@@ -1,23 +1,26 @@
 import { Article, articleSchema } from './types';
+import { ValidationError } from 'joi';
 import * as fs from "fs";
 import * as path from 'path';
 import { parse } from 'csv-parse';
 
-const convertCsvToTypedArray = async (filename: string): Promise<Article[]> => {
+const convertCsvToTypedArray = async (filename: string): Promise<{ articles: Article[], errors: ValidationError[] }> => {
     return new Promise((resolve, reject) => {
         const articles: Article[] = [];
+
+        const errorsArray: ValidationError[] = [];
 
         fs.createReadStream(filename).pipe(parse({ delimiter: ',' })).on('data', (row: any) => {
             const [title, author, publisher, url, tags, wordCount, inQueue, favorited, read, highlightCount, lastInteractionDate, id] = row;
 
             const rowObject = {
                 id,
-                title, 
-                author, 
-                publisher, 
+                title,
+                author,
+                publisher,
                 url,
                 tags,
-                wordCount, 
+                wordCount,
                 inQueue,
                 favorited,
                 read,
@@ -28,10 +31,14 @@ const convertCsvToTypedArray = async (filename: string): Promise<Article[]> => {
             const validationResult = articleSchema.validate(rowObject);
 
             if (validationResult.error) {
-                console.log('Validation Error: ', validationResult.error);
+                errorsArray.push(validationResult.error);
             } else {
                 articles.push(rowObject);
             }
+        }).on('end', () => {
+            resolve({ articles, errors: errorsArray });
+        }).on('error', (err) => {
+            reject(err)
         })
     });
 }
@@ -39,9 +46,12 @@ const convertCsvToTypedArray = async (filename: string): Promise<Article[]> => {
 const main = async () => {
     const absolutePath = path.resolve(__dirname, '../..');
 
-    const filePath = path.join(absolutePath, '_matter_history.csv');  
+    const filePath = path.join(absolutePath, '_matter_history.csv');
 
-    await convertCsvToTypedArray(filePath);
+    const results = await convertCsvToTypedArray(filePath);
+
+    console.log('articles: ', results.articles.length);
+    console.log('errors: ', results.errors.length)
 }
 
 main();
