71c41494f012b1a8f560ff9ca978c0cf704e4868
Jimmy Cerone <67113663+jimmyceroneii@users.noreply.github.com>
2024-04-20 14:06:01 +0000

    automate retreival of newsletter articles

--- weekly-newsletter/src/sources/readwise/index.ts
+++ weekly-newsletter/src/sources/readwise/index.ts
@@ -6,14 +6,26 @@ type RetrieveReadwiseArticlesReturnType = {
     weeklyArticles: ReadwiseArticle[];
 }
 
-const DATE_TAG = '2024-04-04';
+const getDateTag = () => {
+    const date = new Date();
+
+    const year = date.getFullYear();
+
+    const month = date.getMonth() + 1;
+
+    const day = date.getDate();
+
+    return `${year}-${month}-${day}`
+}
 
 export const retrieveReadwiseArticle = async (): Promise<RetrieveReadwiseArticlesReturnType> => {
+    const dateTag = getDateTag();
+
     const articles = await fetchDocumentListApi();
 
     console.log('items before filtering: ', articles.length)
 
-    const filteredArticles: ReadwiseArticle[] = articles.filter((article) => isWeeklyNewsletter(article, DATE_TAG));
+    const filteredArticles: ReadwiseArticle[] = articles.filter((article) => isWeeklyNewsletter(article, dateTag));
 
     console.log('found weekly articles: ', filteredArticles.length);
 
