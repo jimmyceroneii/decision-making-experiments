8227b3f2a8356bbba31222a04e5f9c90fb234304
jimmyceroneii <jimmy.cerone@gmail.com>
2024-07-08 20:07:04 -0400

    initial randomization check

--- random-distribution-test/randomization.ts
+++ random-distribution-test/randomization.ts
@@ -12,7 +12,7 @@ if (!matterArticles) {
 const randomizationTest = async () => {
 	const selected: SelectedElements = {};
 
-	for (let i = 0; i < 10000; i++) {
+	for (let i = 0; i < 1000; i++) {
 		const matterArticle = await retrieveRandomMatterArticle(matterArticles);
 
 		const matterArticleUrl = matterArticle.url;
@@ -24,7 +24,13 @@ const randomizationTest = async () => {
 		}
 	}
 
-	console.log("selected: ", JSON.stringify(selected));
+	const selectedArray = Object.entries(selected);
+
+	const sortedSelection = selectedArray.sort((a, b) => b[1] - a[1]);
+
+	for (const selection of sortedSelection) {
+		console.log(`${selection[0]}: ${selection[1]}`);
+	}
 };
 
 const main = async () => {
