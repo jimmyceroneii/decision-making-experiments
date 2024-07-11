6fbcb71d21b6fa27502f7cde7f2309ca0d73e47a
jimmyceroneii <jimmy.cerone@gmail.com>
2024-07-10 20:35:53 -0400

    add error handling to reflect

--- experiments/newsletter/append-to-reflect.ts
+++ experiments/newsletter/append-to-reflect.ts
@@ -7,17 +7,24 @@ export const appendToReflect = async (text: string) => {
 	try {
 		const today = `${new Date().toLocaleDateString()} Newsletter`;
 
-		await fetch("https://reflect.app/api/graphs/jimmyceroneii/notes", {
-			method: "POST",
-			headers: {
-				Authorization: `Bearer ${REFLECT_ACCESS_TOKEN}`,
-				"Content-Type": "application/json",
+		const result = await fetch(
+			"https://reflect.app/api/graphs/jimmyceroneii/notes",
+			{
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${REFLECT_ACCESS_TOKEN}`,
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					subject: today,
+					content_markdown: text,
+				}),
 			},
-			body: JSON.stringify({
-				subject: today,
-				content_markdown: text,
-			}),
-		});
+		);
+
+		if (result.status !== 200) {
+			console.log("error in writing to reflect: ", result.status);
+		}
 	} catch (e) {
 		console.log("error when adding to reflect: ", e);
 		throw e;
