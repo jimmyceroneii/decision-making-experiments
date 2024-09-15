d358a226eaed37a8988cf8b94a8eac31616a9f79
jimmyceroneii <jimmy.cerone@gmail.com>
2024-09-15 09:44:18 -0400

    return list of backlinks from reflect

--- experiments/deep-dive/reflect-to-links.ts
+++ experiments/deep-dive/reflect-to-links.ts
@@ -11,18 +11,55 @@ type ReflectDocument = {
 	backlinked_count: number;
 };
 
+type ReflectBlock = {
+	type: string;
+	attrs: Record<string, unknown>;
+	content: ReflectContent | ReflectContent[];
+};
+
+interface ReflectContent {
+	type: string;
+	text: string;
+	attrs: Record<string, unknown>;
+	content?: ReflectContent[];
+};
+
+interface ReflectBacklink extends ReflectContent {
+	type: 'backlink';
+	attrs: {
+		id: string;
+		label: string;
+		graphId: string;
+	}
+}
+
+const findBackLinks = (content: ReflectContent | ReflectContent[]): ReflectBacklink[] => {
+	if (Array.isArray(content)) {
+        return content.flatMap(findBackLinks);
+    } else if (Array.isArray(content.content)) {
+		return content.content.flatMap(findBackLinks);
+	}  else if (content.type === 'backlink') {
+        return [content as ReflectBacklink];
+    } else {
+		return [];
+	}    
+};
+
 const main = async () => {
 	try {
 		const filePath =
 			"/Users/personal/Documents/decision-making-projects/writing-randomizer/writing-cache/last-week-i-learned/deep-dives/home-robotics.json";
 		const fileContent = await fs.readFile(filePath, "utf8");
-		const jsonData = JSON.parse(fileContent);
 
-		const jsonContent = jsonData.document_json;
-		const parsedContent = JSON.parse(jsonContent);
+		const reflectDocument: ReflectDocument = JSON.parse(fileContent);
 
-		const content = parsedContent.content;
-		console.log("Content:", content);
+		const jsonContent = reflectDocument.document_json;
+		const parsedReflectBlock: ReflectBlock = JSON.parse(jsonContent);
+
+		const content: ReflectContent[] | ReflectContent = parsedReflectBlock.content;
+		
+		const backlinks = findBackLinks(content);
+		console.log("Backlinks:", backlinks);
 	} catch (error) {
 		console.error("Error reading file:", error);
 	}
