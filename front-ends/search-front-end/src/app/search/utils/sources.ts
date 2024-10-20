e9f8203ce02d1b4e378f5b137611744c392b51e6
jimmyceroneii <jimmy.cerone@gmail.com>
2024-10-20 17:18:23 -0400

    switch to yarn, start refactor

--- front-ends/search-front-end/src/app/search/utils/sources.ts
+++ front-ends/search-front-end/src/app/search/utils/sources.ts
@@ -2,7 +2,7 @@ import { MatterArticle } from "./types";
 
 export const fetchLocalMatterArticles = (): MatterArticle[] | undefined => {
   try {
-    const data = require("../../../../../backend/sources/matter/backup.json");
+    const data = require('../../../../../../backend/sources/matter/backup.json');
     return data as MatterArticle[];
   } catch (err) {
     console.error(err);
